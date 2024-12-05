5380e05684da4dc0514cf6bc7a238e552826df89
me <findaston@gmail.com>
2024-12-05 16:42:27 -0500

    ruff formatted

--- py_src/auth.py
+++ py_src/auth.py
@@ -12,7 +12,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db #type: ignore
+from flaskr.db import get_db  # type: ignore
 
 # arguments: name of bp, location
 bp = Blueprint("auth", __name__, url_prefix="/auth")
