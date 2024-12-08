6ec9430906b9038b5b0460792608b9e9bbbe686b
me <findaston@gmail.com>
2024-12-08 18:00:57 -0500

    cleared the original file and added potential functions that may get used for the login pages, currently commented out

--- py_src/static/script.js
+++ py_src/static/script.js
@@ -1,56 +1,15 @@
+//const login = document.getElementById("signin")
+//login.addEventListener("clicK")
 
 
-var campusMapButton = document.getElementById("campusMapButton");
-campusMapButton.onclick = function() {
-  location.href = "MAP.html";
-};
 
-var logoutButton = document.getElementById("logoutButton");
-logoutButton.oncliick = function() {
-  location.href = "menu.html";
-}
+/*const registerform = document.getElementById("register")
+loginform.addEventListener("submit", function(event){
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-var reviewButton = document.getElementById("reviewButton");
-reviewButton.onclick = function() {
-  location.href = "review.html";
-}
-
-var cancelReviewButton = document.getElementById("cancelReviewButton");
-cancelReviewButton.onclick = function() {
-  location.href = "menu.html";
-}
-
-var submitReview = document.getElementById("submitReview");
-submitReview.onclick = function() {
-  location.href = "menu.html";
-}
-
-var signUp = document.getElementByID("signUp");
-signUp.onclick = function() {
-  location.href = "menu.html";
-}
-
-var signIn = document.getElementById("signIn");
-signIn.onclick = function() {
-  location.href= "menu.html";
-}
-
-var currentReview = document.getElementById("currentReview");
-currentReview.onclick = function() {
-  location.href = "currentReview.html"
-}
-
-var backButton = document.getElementById("backButton");
-backButton.onclick = function() {
-  location.href = "menu.html"
-}
-
-var signUp = document.getElementById("signUp");
-signUp.onclick = function() {
-  location.href = "menu.html"
-}
-
-var mapBack = document.getElementById("mapBack");
-mapBack.onclick = function() {
-  location.href = "menu.html"
-}
+    if(!username || !password) {
+        alert("Please fill out all fields");
+        event.preventDefault();
+    }
+}); */
