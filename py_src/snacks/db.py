a55b07d92d8dc0e6f9774635f7af63e7ab5e2b03
me <findaston@gmail.com>
2025-02-08 22:33:25 -0500

    ruff formatted

--- py_src/snacks/db.py
+++ py_src/snacks/db.py
@@ -1,11 +1,12 @@
-import os #noqa 
-from sqlmodel import SQLModel, Field, create_engine, Session, Relationship, select 
+import os  # noqa
+from sqlmodel import SQLModel, Field, create_engine, Session, Relationship, select
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-@asynccontextmanager 
+
+@asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
     # inserts the locations only once.
@@ -15,6 +16,7 @@ async def lifespan(app: FastAPI):
             create_locations()
     yield
 
+
 app = FastAPI(lifespan=lifespan)
 
 
@@ -22,22 +24,22 @@ app.mount("/static", StaticFiles(directory="py_src/static"), name="static")
 templates = Jinja2Templates(directory="py_src/templates")
 
 
-
-#to be used in future versions
-'''class User(SQLModel, table=True):
+# to be used in future versions
+"""class User(SQLModel, table=True):
     __tablename__ = "users"
 
     user_id: int | None = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True, nullable=False)
     password: str = Field(nullable=False)
     reviews: list["Review"] = Relationship(back_populates="author")
-'''
+"""
+
 
 class Review(SQLModel, table=True):
     __tablename__ = "reviews"
 
     review_id: int | None = Field(default=None, primary_key=True)
-    #author_id: int = Field(nullable=False, foreign_key="users.user_id")
+    # author_id: int = Field(nullable=False, foreign_key="users.user_id")
     location_id: int = Field(nullable=False, foreign_key="locations.location_id")
     title: str = Field(nullable=False)
     body: str = Field(nullable=False)
@@ -46,7 +48,7 @@ class Review(SQLModel, table=True):
     # created: datetime = Field(nullable=False) Field(sa_column=Column(TIMESTAMP(timezone=True),
     # nullable=False, server_default=text("now()")))
     # link values
-    #author: "User" = Relationship(back_populates="reviews") future for linking
+    # author: "User" = Relationship(back_populates="reviews") future for linking
     location: "Location" = Relationship(back_populates="reviews")
     nickname: str = Field(nullable=False)
 
@@ -58,11 +60,13 @@ class Location(SQLModel, table=True):
     name: str = Field(default=None, nullable=False, index=True)
     reviews: list["Review"] = Relationship(back_populates="location")
 
+
 sqlite_db_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_db_name}"
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args) 
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
 
 def create_locations():
     location_1 = Location(name="Argo Tea")
@@ -80,7 +84,6 @@ def create_locations():
         session.commit()
 
 
-
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
@@ -88,4 +91,3 @@ def create_db_and_tables():
 def get_session():
     with Session(engine) as session:
         yield session
-
