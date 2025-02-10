563f5f8e916381cec41690a72ed5f5e29dd3a500
me <findaston@gmail.com>
2025-02-10 01:47:18 -0500

    updated root test function

--- py_tests/test_routes.py
+++ py_tests/test_routes.py
@@ -8,11 +8,91 @@ from snacks.db import Review, Location, get_session
 
 client = TestClient(app)
 
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine("sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool)
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+
+"""def test_register(client: TestClient, session: Session):
+    response = client.post(
+        "/register", data={"username": "aston", 
+                           "password": "test"},
+                           follow_redirects=False,
+    )
+    assert response.status_code == 303
+    assert response.headers["location"] == "auth/login"
+
+    user = session.exec(select(User).where(User.username == "aston")).first()
+    assert user is not None 
+    """
 
 # login pages
-def test_root():
-    response = client.get("/")
+def test_root(session: Session, client: TestClient):
+    """user1_data = {"username": "test", "password": "test password"}
+    user1 = User(**user1_data)
+
+    user2_data = {"username": "aston", "password": "test"}
+    user2 = User(**user2_data)
+
+    session.add(user1)
+    session.add(user2)
+    session.commit()
+    """
+    # locations
+    location1 = Location(name="test lo")
+    location2 = Location(name="dining hall")
+    session.add(location1)
+    session.add(location2)
+    session.commit()
+
+    # reviews
+    review1_data = {
+        # "author_id": user1.user_id,
+        "location_id": location1.location_id,
+        "rating": 3,
+        "title": "Food",
+        "body": "Great food",
+        "nickname": "test",
+    }
+    review2_data = {
+        # "author_id": user2.user_id,
+        "location_id": location2.location_id,
+        "rating": 2,
+        "title": "chicken",
+        "body": "test body",
+        "nickname": "test2",
+    }
+    review1 = Review(**review1_data)
+    review2 = Review(**review2_data)
+    session.add(review1)
+    session.add(review2)
+    session.commit()
+
+    response = client.get("/menu")
     assert response.status_code == 200
+
+    assert "Food" in response.text
+    assert "chicken" in response.text
+    assert "test" in response.text
+    assert "test2" in response.text
+    # assert "test" in response.text
+    # assert "aston" in response.text
+
 
 
 """def test_login():
@@ -53,38 +133,6 @@ def test_wils_leave_review():
 
 
 # create user
-@pytest.fixture(name="session")
-def session_fixture():
-    engine = create_engine("sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool)
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
-
-
-@pytest.fixture(name="client")
-def client_fixture(session: Session):
-    def get_session_override():
-        return session
-
-    app.dependency_overrides[get_session] = get_session_override
-
-    client = TestClient(app)
-    yield client
-    app.dependency_overrides.clear()
-
-
-"""def test_register(client: TestClient, session: Session):
-    response = client.post(
-        "/register", data={"username": "aston", 
-                           "password": "test"},
-                           follow_redirects=False,
-    )
-    assert response.status_code == 303
-    assert response.headers["location"] == "auth/login"
-
-    user = session.exec(select(User).where(User.username == "aston")).first()
-    assert user is not None 
-    """
 
 
 # create review
